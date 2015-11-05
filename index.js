dd7a5fe372a0490d08c83caa94d505e9f77f21f1
aequanimitas <htavenido@gmail.com>
2015-11-05 23:17:25 +0800

    fix diy cli

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var http = require('http'),
     moment = require('moment'),
     redisClient = redis.createClient(),
     seed = require('./seed.json'),
-    availableSymbols = seed.map(function(x) { return x.symbol });
+    availableSymbols = seed.map(function(x) { return x.symbol }),
+    bbUrl = 'http://www.bloomberg.com/markets/chart/data/1D/',
+    marketSymbol = 'PM';
 
 function threeMinGap(x,y) {
   return (x - y) > 18000;
 }
 
 function getStockQuote(symbol) {
-  http.request('http://www.bloomberg.com/markets/chart/data/1D/' + symbol + ':PM', function(res) {
+  http.request(bbUrl + symbol + ':' + marketSymbol, function(res) {
    var data = '';
    res
    .on('data', function(c) {
@@ -34,21 +36,48 @@ function symbolExists(x) {
   return availableSymbols.indexOf(x.toUpperCase()) > -1
 }
 
+function usage() {
+  var message = 'Usage: \n';
+  seed.forEach(function(x) {
+    message += x.symbol + '\t' + x.companyName + '\t' + x.sector + '\n';
+  });
+  console.log(message);
+}
+
+function argHandler() {
+  var x, args = process.argv.slice(2);
+  if (args.length == 0) {
+    usage();
+    return undefined;
+  } else {
+    x = args[0];
+    return x;
+  }
+}
+
 function init() {
-  var x = process.argv.slice(2)[0];
-  if(symbolExists(x)) {
-    redisClient.get('psex:lastCheck', function(err, reply) {
-      if (!err) {
-        if(threeMinGap(parseInt(moment(new Date()).format('x'), 10), parseInt(reply, 10))) {
-          getStockQuote(x);
+  var x = argHandler();
+  if (x) {
+    if(symbolExists(x)) {
+      redisClient.get('psex:lastCheck', function(err, reply) {
+        if (!err) {
+          if(threeMinGap(parseInt(moment(new Date()).format('x'), 10), parseInt(reply, 10))) {
+            getStockQuote(x);
+          } else {
+            console.log('Wait for three more minutes');
+            redisClient.quit();
+          }
         } else {
-          console.log('Wait for three more minutes');
+          console.log('There was an error: ' + err);
           redisClient.quit();
         }
-      } else {
-        console.log('There was an error: ' + err);
-      }
-    });
+      });
+    } else {
+      usage();
+      redisClient.quit();
+    }
+  } else {
+    redisClient.quit(); 
   }
 }
 
